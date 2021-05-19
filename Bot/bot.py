9cc462e3377dd7f220efad0d8ac19ff61a72283d
Sankar Vinayak <e.p.sankarvinayak@gmail.com>
2021-05-19 20:23:07 +0530

    Added replay and tweet

--- Bot/bot.py
+++ Bot/bot.py
@@ -33,16 +33,31 @@ def get_thread_text(api, author_id, status_id):
     return thread
 
 
+def is_tweet(mention):
+    if 'tweet' in mention.full_text.lower():
+        return 1
+    else:
+        return 0
+
+
+def get_tweet_text(api, tweet_id):
+    tweet_list = [tweet_id]
+    tweet = api.statuses_lookup(tweet_list)
+    tweet_text = tweet[0].text
+    return tweet_text
+
+
 def get_mentioned_thread(api, last_seen_id_thread):
     mentions = api.mentions_timeline(last_seen_id_thread, tweet_mode='extended')
     if not mentions:
         return 0, 0, 0, 0
     mention = mentions[-1]
+    is_it_tweet = is_tweet(mention)
     last_seen_id_thread = mention.id
     status_id_thread = mention.in_reply_to_status_id
     author_id_thread = mention.in_reply_to_user_id
-    user_id_thread = api.get_user(mention.id)
-    return last_seen_id_thread, status_id_thread, author_id_thread, user_id_thread
+    user_id_thread = mention.user.id
+    return last_seen_id_thread, status_id_thread, author_id_thread, user_id_thread, is_it_tweet
 
 
 def get_last_seen_id():
--- main.py
+++ main.py
@@ -9,9 +9,13 @@ if __name__ == '__main__':
     auth_api = authenticate()
     while True:
         last_seen_id = get_last_seen_id()
-        last_seen_id, status_id, author_id, user = get_mentioned_thread(auth_api, last_seen_id)
+        last_seen_id, status_id, author_id, user,is_it_tweet = get_mentioned_thread(auth_api, last_seen_id)
         if last_seen_id == 0 and status_id == 0 and author_id == 0 and user == 0:
             continue
         set_last_seen_id(last_seen_id)
-        thread = get_thread_text(auth_api, author_id, status_id)
+        if is_it_tweet==1:
+            thread = get_tweet_text(auth_api,status_id)
+        else:
+            thread = get_thread_text(auth_api, author_id, status_id)
+        auth_api.send_direct_message(user,thread)
         update_db(thread, user)
