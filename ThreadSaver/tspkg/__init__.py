759c6c6a5bb388fdc8ae530e4f60d68b6fca72ea
alwin48 <alwinjoseph48@gmail.com>
2021-05-23 10:58:48 +0530

    Added Login with twitter and Database

--- ThreadSaver/tspkg/__init__.py
+++ ThreadSaver/tspkg/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_dance.contrib.twitter import make_twitter_blueprint
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dude280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../site.db'
+
+twitter_bp = make_twitter_blueprint(api_key='api key here', api_secret='api secret key here')  # main account api
+app.register_blueprint(twitter_bp, url_prefix="/login")
+
 db = SQLAlchemy(app)
 
 from tspkg import routes
--- ThreadSaver/tspkg/models.py
+++ ThreadSaver/tspkg/models.py
@@ -1,12 +1,10 @@
 from tspkg import db
-from flask_login import UserMixin
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(20), unique=True, nullable=False)
-    tweet = db.Column(db.Text, nullable=False)
-    screen_name = db.Column(db.String(60), nullable=False)
+    user_id = db.Column(db.String(20))
+    tweet = db.Column(db.String(2560))
 
     def __repr__(self):
-        return f"User('{self.user_id}', '{self.tweet}', '{self.screen_name}')"
+        return f"User('{self.user_id}', '{self.tweet}')"
