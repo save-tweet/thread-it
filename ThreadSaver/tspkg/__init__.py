9a908172eb8e40094ef68e20d6321fdcc41d62c2
Sankar Vinayak <e.p.sankarvinayak@gmail.com>
2021-05-24 13:35:38 +0530

    small changes

--- ThreadSaver/tspkg/__init__.py
+++ ThreadSaver/tspkg/__init__.py
@@ -1,3 +1,5 @@
+from os import environ
+
 from flask import Flask
 from flask_dance.contrib.twitter import make_twitter_blueprint
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +8,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dude280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../site.db'
 
-twitter_bp = make_twitter_blueprint(api_key='rsixXRkxjRpEiZnIhMDy016UB', api_secret='M04dEu0aBeX1ExvzTHR8jcS6N4dq4Pfn76kozLp7LmRORrFl8d')  # main account api
+Consumer_key_web = environ['Consumer_key_web']
+Consumer_key_secret_web = environ['Consumer_key_secret_web']
+twitter_bp = make_twitter_blueprint(Consumer_key_web, Consumer_key_secret_web)  # main account api
 app.register_blueprint(twitter_bp, url_prefix="/login")
 
 db = SQLAlchemy(app)
